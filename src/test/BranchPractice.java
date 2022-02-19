2c841c38aa56ed481c500e634e270d099785d685
andrewnova <tech.easy1@outlook.com>
2022-02-19 14:53:41 -0600

    My second branch change!

--- src/test/BranchPractice.java
+++ src/test/BranchPractice.java
@@ -3,6 +3,7 @@ package test;
 public class BranchPractice {
     public static void main(String[] args) {
         System.out.println("My branch Practice");
+        System.out.println("Knowing git is fun");
     }
 
 }
